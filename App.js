571bd999ddfdbf99a0b9210aaf44ff50be69e8a5
Gerard Le Forban <gerard.leforban@free.fr>
2021-08-10 00:05:20 +0200

    import icones

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// import { Icon } from 'react-native-elements';
+import { Icon as Icon_ne } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -12,6 +17,20 @@ function HomeScreen({navigation}) {
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
+      <Icon_ne name='fingerprint' size={50} />
+      <Icon_ne name='rowing' />
+      <Icon_ne name='g-translate' color='#00aced' />
+      <Icon_ne name='rowing' />
+      <FontAwesome5 name={'accessible-icon'} solid size={50} />
+      <FontAwesome5 name={'allergies'} solid size={50} />
+      <Icon name="500px" size={50} color="#900" />
+      <Icon.Button
+        name="facebook"
+        backgroundColor="#3b5998"
+        onPress={""}
+      >
+        Login with Facebook
+      </Icon.Button>
     </View>
   );
 }
