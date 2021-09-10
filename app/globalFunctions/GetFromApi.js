d5ca4fd3856136069ae3347e4bf0aa1a46107565
Gerard Le Forban <gerard.leforban@free.fr>
2021-09-10 14:30:20 +0200

    timeOut sur get user from api

--- app/globalFunctions/GetFromApi.js
+++ app/globalFunctions/GetFromApi.js
@@ -101,7 +101,7 @@ const getSiteAndEquipementsByIdSiteFromApi = async (idSite,signal) => {
   let site_sc = site;
   try {
     //const response = await fetch('http://192.168.1.13:8000/api/sites/1');
-    const response = await fetch(adresseServeur + '/api/sites/' + idSite, {signal});
+    const response = await fetch(adresseServeur + '/api/sites/' + idSite, {signal : signal});
     const json = await response.json();
     site_sc.push(json);
     let lesEquipements = [];
@@ -130,7 +130,7 @@ const getSiteAndEquipementsByIdSiteFromApi = async (idSite,signal) => {
 const getEquipementAndPtMesureByIdEquiFromApi = async (idEqui,signal) =>{
   let equipements_sc = equipements;
   try {
-    const response = await fetch(adresseServeur + '/api/equipements/' + idEqui, {signal});
+    const response = await fetch(adresseServeur + '/api/equipements/' + idEqui, {signal : signal});
     const json = await response.json();
     equipements_sc.push(json);
     let lesPointsDeMesures = [];
@@ -154,7 +154,7 @@ const getEquipementAndPtMesureByIdEquiFromApi = async (idEqui,signal) =>{
 const getPtDeMesAndCapteurByIdPtdeMesFromApi = async (idPtMes,signal) =>{
   let pointsMesures_sc = pointsMesures;
   try {
-    const response = await fetch(adresseServeur + '/api/pt_mesures/' + idPtMes, {signal});
+    const response = await fetch(adresseServeur + '/api/pt_mesures/' + idPtMes, {signal : signal});
     const json = await response.json();
     pointsMesures_sc.push(json);
     let leCapteur = "";
@@ -176,7 +176,7 @@ const getPtDeMesAndCapteurByIdPtdeMesFromApi = async (idPtMes,signal) =>{
 const getCapteurByIdFromApi = async (idCapteur,signal) =>{
   let capteurs_sc = capteurs;
   try {
-    const response = await fetch(adresseServeur + '/api/capteurs/' + idCapteur, {signal});
+    const response = await fetch(adresseServeur + '/api/capteurs/' + idCapteur, {signal : signal});
     const json = await response.json();
     capteurs_sc.push(json);
   } catch (error) {
@@ -191,7 +191,7 @@ const getCapteurByIdFromApi = async (idCapteur,signal) =>{
 const getGrandeursFromApi = async (signal) =>{
   let grandeurs_sc = grandeurs;
   try {
-    const response = await fetch(adresseServeur + '/api/grandeurs', {signal});
+    const response = await fetch(adresseServeur + '/api/grandeurs', {signal : signal});
     const json = await response.json();
     grandeurs_sc.push(json);
   } catch (error) {
@@ -213,11 +213,20 @@ export const getUsersFromApi = async () => {
   let signalGu = controllerGu.signal; // propriété pour insérer dans le fetch
   let fetchOk = false; // indicateur pour fetch terminé?
   // time out de 15 secondes sur le fetch
-  setTimeout(() => {fetchOk ? null:controllerGu.abort();}, 15000);
+  setTimeout(() => {
+    // fetchOk ? null:controllerGu.abort()
+    if (fetchOk == false) {
+      controllerGu.abort();
+      // console.log("timeout");
+      // console.log(fetchOk);
+    }
+  },
+    15000);
+
   tableauUsers = [];
   try {
   //  const response = await fetch('https://reactnative.dev/movies.json');
-    const response = await fetch(adresseServeur + '/api/utilisateurs?page=1', signalGu);
+    const response = await fetch(adresseServeur + '/api/utilisateurs?page=1', {signal: signalGu});
     const json = await response.json();
     console.log('dans getUsersFromApi');
     // console.log(json);
